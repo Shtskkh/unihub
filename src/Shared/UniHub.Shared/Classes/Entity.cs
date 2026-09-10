aa1adb31eec647f6db9cb423fefd795e6898cf1d
Alexey Shatskikh <shtskkh@gmail.com>
2026-09-10 15:45:18 +0700

    shared: добавлен класс Entity.

---
+++ src/Shared/UniHub.Shared/Classes/Entity.cs
@@ -0,0 +1,39 @@
+namespace UniHub.Shared.Classes;
+
+public abstract class Entity<TId> : IEquatable<Entity<TId>>
+    where TId: struct, IEquatable<TId>
+{
+    public TId Id { get; protected init; }
+
+    // Для EF Core
+    protected Entity()
+    {
+    }
+
+    protected Entity(TId id)
+    {
+        Id = id;
+    }
+
+    protected virtual bool IsTransient => Id.Equals(default);
+
+    public bool Equals(Entity<TId>? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        if (IsTransient || other.IsTransient) return false;
+        if (GetType() != other.GetType()) return false;
+
+        return Id.Equals(other.Id);
+    }
+
+    public override bool Equals(object? obj) => Equals(obj as Entity<TId>);
+
+    public override int GetHashCode() => HashCode.Combine(GetType(), Id);
+
+    public override string ToString() => $"{GetType().Name} [Id={Id}]";
+
+    public static bool operator ==(Entity<TId>? left, Entity<TId>? right) => left is null ? right is null : left.Equals(right);
+
+    public static bool operator !=(Entity<TId>? left, Entity<TId>? right) => !(left == right);
+}
