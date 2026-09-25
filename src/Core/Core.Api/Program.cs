effef47499cf9c66788faa4ae26e48164a13ff65
Alexey Shatskikh <shtskkh@gmail.com>
2026-09-25 13:57:15 +0700

    core: сконфигурирован scalar.

--- src/Core/Core.Api/Program.cs
+++ src/Core/Core.Api/Program.cs
@@ -1,11 +1,20 @@
 using Core.Infrastructure;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCoreInfrastructure(builder.Configuration);
 
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference("/docs");
+}
 
 app.Run();
