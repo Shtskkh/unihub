2d978943db1164eea9588faf0bc630e63c8090cc
Alexey Shatskikh <shtskkh@gmail.com>
2026-09-25 13:41:16 +0700

    core: Migrator.

---
+++ src/Core/Core.Migrator/Program.cs
@@ -0,0 +1,8 @@
+﻿using Core.Infrastructure;
+using Shared.Migrator;
+
+await MigratorHost.RunAsync<CoreDbContext>(
+    args,
+    CoreDbContext.DefaultSchema,
+    CoreDbContext.DefaultConnectionStringName
+);
